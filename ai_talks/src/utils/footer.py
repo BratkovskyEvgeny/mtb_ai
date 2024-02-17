69453e7935aa56bf9d4b2ad6e1b06254332acfdc
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2024-02-17 18:04:07 +0300

    Update footer.py

--- ai_talks/src/utils/footer.py
+++ ai_talks/src/utils/footer.py
@@ -29,8 +29,7 @@ def show_info(icon: Path) -> None:
         #- {render_svg(icon)} [{st.session_state.locale.footer_channel}](https://https://t.me/Br_Evgeny)
        
     #""", unsafe_allow_html=True)
-    st.markdown(f"<div style='text-align: justify;'>{st.session_state.locale.responsibility_denial}</div>",
-                unsafe_allow_html=True)
+    st.markdown(f"<div style='text-align: justify;'>{st.session_state.locale.responsibility_denial}</div>", unsafe_allow_html=True)
     st.divider()
     #st.markdown(f"project [repo on github]({REPO_URL}) waiting for your :star: | [report]({BUG_REPORT_URL}) a bug")
 
