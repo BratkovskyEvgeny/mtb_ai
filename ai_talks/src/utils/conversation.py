d7bfe7072bd9461fe4d0d740e39cf45efd9ea43b
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2024-02-20 01:46:38 +0300

    Update conversation.py

--- ai_talks/src/utils/conversation.py
+++ ai_talks/src/utils/conversation.py
@@ -37,7 +37,7 @@ def show_chat_buttons() -> None:
     b0, b1, b2 = st.columns(3)
     with b0, b1, b2:
         b0.button(label=st.session_state.locale.chat_run_btn)
-        b1.button(label=st.session_state.locale.chat_clear_btn, on_click=clear_chat, disabled =True)
+        b1.button(label=st.session_state.locale.chat_clear_btn, on_click=clear_chat)
         b2.download_button(
             label=st.session_state.locale.chat_save_btn,
             data="\n".join([str(d) for d in st.session_state.messages[1:]]),
